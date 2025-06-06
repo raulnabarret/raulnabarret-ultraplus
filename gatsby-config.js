616cfbf95db05df92d6b93f0ecc6dc93864a91ca
raulnabarret <raulnabarret@gmail.com>
2025-06-06 16:43:33 -0600

    Added fonts for project

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,25 @@
 module.exports = {
   siteMetadata: {
     title: `ultraplus`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.aquaultraplus.com`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Saira`,
+            file: `https://fonts.googleapis.com/css2?family=Saira:wght@700&display=swap`,
+          },
+          {
+            name: `Open Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
 }
