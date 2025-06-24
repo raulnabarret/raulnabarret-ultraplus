9d985eb3cf8f213e4ce72cce1ec33a63aa9c14b6
raulnabarret <raulnabarret@gmail.com>
2025-06-24 15:20:16 -0600

    Added banner section

---
+++ src/components/banner/banner.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import * as styles from "./banner.module.css"
+
+function Banner() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.headerContainer}>
+                <h1>Aqua Ultra Plus</h1>
+            </div>
+            <div className={styles.textContainer}>
+                <p>Somos una purificadora de agua potable con más de 30 años de servicio en Tampico. <br></br>
+                    Ofrecemos venta de agua purificada al público en general en nuestra sucursal, 
+                    así como servicio a domicilio en tu hogar y oficina o negocio.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 import Navbar from "../components/navbar"
+import Banner from '../components/banner/banner';
 
 const IndexPage = () => {
 	return (
 		<main>
 			<Navbar></Navbar>
+			<Banner></Banner> 
 		</main>
 	)
 }
