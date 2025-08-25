0be298023619e3e4fc5d88060f75ff56d9c6ee09
raulnabarret <raulnabarret@gmail.com>
2025-08-25 10:38:59 -0600

    Updated meta description and index text

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -8,7 +8,7 @@ function Banner() {
                 <h1>Aqua Ultra Plus</h1>
             </div>
             <div className={styles.textContainer}>
-                <p>Somos una purificadora de agua potable con más de 30 años de servicio en Tampico. <br></br>
+                <p>Aqua Ultra Plus es una purificadora de agua potable con más de 30 años de servicio en Tampico, Tamaulipas. <br></br>
                     Ofrecemos venta de agua purificada al público en general en nuestra sucursal, 
                     así como servicio a domicilio en tu hogar y oficina o negocio.
                 </p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,9 +29,7 @@ export const Head = () => (
 		<meta name="google-site-verification" content="A26tHKjKCnwYUOAUeQOoyvlvap_V9sfvKbu7Vc9qeaA" />
 		<meta 
 			name="description" 
-			content="Aqua Ultra Plus es una purificadora de agua potable con más de 30 años de servicio en Tampico.
-			Ofrecemos venta de agua purificada al público en general en nuestra sucursal, 
-			así como servicio a domicilio en tu hogar y oficina o negocio." 
-		/>
+			content="Aqua Ultra Plus es una purificadora de agua potable con más de 30 años de servicio en Tampico, Tamaulipas. 
+			Ofrecemos venta de agua purificada al público en general en nuestra sucursal, así como servicio a domicilio en tu hogar y oficina o negocio."/>
 	</>
 )
